33dc8ee627c39ec8fa2b95022f7afcfa759bde8c
Marc Bria <marc.bria@gmail.com>
2017-08-27 00:29:55 +0200

    Divide code in generic Dockerfile and a folder for each journal. Added sample folder "journal". Added script to create journals.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# export $OJS_BRANCH="ojs-stable-3_0_2"
-echo "Right now the configuration is in .env file."
-echo "Modify this file to fit your needs and run:"
-echo "> docker-compose -p myJournal up -d"
-# docker-compose up
